5b9f7b36daff45bbb4f60d616d59a0404a87a532
Raiver0f <r1ver0ffundm4nt@gmail.com>
2025-01-21 11:13:24 +0100

    change title

--- apps/client/app/board/[id]/layout.tsx
+++ apps/client/app/board/[id]/layout.tsx
@@ -1,13 +1,5 @@
-import { Metadata } from "next";
 import { FC, PropsWithChildren } from "react";
 import { LifeProvider } from "@/providers/life-provider";
-import { authFetch } from "@/lib/auth-fetch";
-
-export const metadata: Metadata = {
-  title: "Liveblocks",
-};
-
-const publicApiKey = process.env.NEXT_PUBLIC_LIFEBLOCKS_PUBLIC_KEY!;
 
 interface BoardLayoutProps {
   params: { id: string };
